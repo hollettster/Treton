1875942255dfd01fcf8522c12e83a8750453be4e
Adam Hollett <hollettster@gmail.com>
2023-07-10 11:18:37 -0400

    Initial Commit

---
+++ Treton/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Treton
+//
+//  Created by Adam Hollett on 7/10/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Treton/TretonApp.swift
@@ -0,0 +1,17 @@
+//
+//  TretonApp.swift
+//  Treton
+//
+//  Created by Adam Hollett on 7/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TretonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
